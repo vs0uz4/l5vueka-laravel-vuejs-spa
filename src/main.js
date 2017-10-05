9fed36535fb4b87c21ff2120644276e36e7fb121
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-10-05 04:35:41 -0300

    Refactored 'main.js' supply needs of new components
    - Import jQuery;
    - Import Bootstrap;
    - Import Axios;
    - Configure Axios baseUrl;

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+window.jQuery = window.$ = require('jquery')
+
+require('bootstrap')
+
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
+
+const http = axios.create({
+    baseURL: 'http://localhost/api/'
+})
+
+Vue.prototype.$http = http
 
 new Vue({
   el: '#app',
